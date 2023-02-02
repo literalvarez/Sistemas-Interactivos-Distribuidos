c85a0b29dbb635c1c9001361d164e5b62d9fed35
Tomas Alvarez <tomas1alvares@gmail.com>
2023-02-02 01:35:02 -0500

    Docs??

--- Cartas/Assets/API.cs
+++ Cartas/Assets/API.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class API : MonoBehaviour
 {
